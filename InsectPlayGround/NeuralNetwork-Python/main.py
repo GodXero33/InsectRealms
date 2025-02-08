b020bbd3098af18aaaa8eeeabdb34ea6c3755f5f
GodXero <shansathish33@gmail.com>
2025-02-08 12:40:16 +0530

    Neural Network start again with full understanding with Python

--- InsectPlayGround/NeuralNetwork-Python/main.py
+++ InsectPlayGround/NeuralNetwork-Python/main.py
@@ -1,15 +1,66 @@
-inputs = [1, 2, 3, 2.5]
-weights1 = [0.2, 0.8, -0.5, 1.0]
-weights2 = [0.5, -0.91, 0.26, -0.5]
-weights3 = [-0.26, -0.27, 0.17, 0.87]
-bias1 = 2
-bias2 = 3
-bias3 = 0.5
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
-output = [
-	inputs[0] * weights1[0] + inputs[1] * weights1[1] + inputs[2] * weights1[2] + inputs[3] * weights1[3] + bias1,
-	inputs[0] * weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] + inputs[3] * weights2[3] + bias2,
-	inputs[0] * weights3[0] + inputs[1] * weights3[1] + inputs[2] * weights3[2] + inputs[3] * weights3[3] + bias3
-]
+nnfs.init()
 
-print(output)
+class ActivationReLU:
+	def forward (self, inputs):
+		self.output = np.maximum(0, inputs)
+
+class ActivationSoftMax:
+	def forward (self, inputs):
+		exp_values = np.exp(inputs - np.max(inputs, axis = 1, keepdims = True))
+		probabilities = exp_values / np.sum(exp_values, axis = 1, keepdims = True)
+		self.output = probabilities
+
+class Loss:
+	def calculate (self, output, y):
+		sample_losses = self.forward(output, y)
+		data_loss = np.mean(sample_losses)
+		return data_loss
+
+class LossCategoricalCrossEntropy (Loss):
+	def forward (self, y_pred, y_true):
+		samples = len(y_pred)
+		y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+		if len(y_true.shape) == 1:
+			correct_confidences = y_pred_clipped[range(samples), y_true]
+		elif len(y_true.shape) == 2:
+			correct_confidences = np.sum(y_pred_clipped * y_true, axis = 1)
+		
+		negative_log_likelihood = -np.log(correct_confidences)
+		return negative_log_likelihood
+	
+
+class LayerDense:
+	def __init__ (self, n_inputs, n_neurons):
+		self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+		self.biases = np.zeros((1, n_neurons))
+
+	def forward (self, inputs):
+		self.output = np.dot(inputs, self.weights) + self.biases
+
+
+
+
+
+X, y = spiral_data(samples = 100, classes = 3)
+
+dense1 = LayerDense(2, 3)
+activation1 = ActivationReLU()
+
+dense2 = LayerDense(3, 3)
+activation2 = ActivationSoftMax()
+
+loss_function = LossCategoricalCrossEntropy()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+loss = loss_function.calculate(activation2.output)
+print(activation2.output)
